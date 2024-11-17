d6cdb5a4936b3b8b284a3f03aa5972397814a509
Jona Enzinger <jona_enzinger@outlook.com>
2024-11-17 12:38:27 +0100

    Cleanup

---
+++ immichserver/immichserver.go
@@ -0,0 +1,103 @@
+package immichserver
+
+import (
+	"context"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"mime"
+	"net/textproto"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/JonaEnz/immich-sync/oapi"
+	"github.com/google/uuid"
+	"github.com/ogen-go/ogen/http"
+	"github.com/ogen-go/ogen/ogenerrors"
+)
+
+type ImmichServer struct {
+	apiURL     string
+	apiKey     string
+	oapiClient *oapi.Client
+}
+
+func NewImmichServer(apiKey, serverURL string) *ImmichServer {
+	client, _ := oapi.NewClient(serverURL, &ImmichServerSecuritySource{
+		key: oapi.APIKey{
+			APIKey: apiKey,
+		},
+	})
+
+	return &ImmichServer{
+		apiURL:     serverURL,
+		apiKey:     apiKey,
+		oapiClient: client,
+	}
+}
+
+func (i *ImmichServer) Upload(path string, assetSha1 *string) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+
+	if assetSha1 == nil {
+		h := sha1.New()
+		if _, err = io.Copy(h, file); err != nil {
+			return err
+		}
+		sha1String := fmt.Sprintf("%x", h.Sum(nil))
+		assetSha1 = &sha1String
+	}
+
+	fileInfo, err := file.Stat()
+	if err != nil {
+		return err
+	}
+	mimename, _, err := mime.ParseMediaType(filepath.Ext(path))
+	if err != nil {
+		return err
+	}
+	mimetype := textproto.MIMEHeader{}
+	mimetype.Set("Content-Type", mimename)
+	assetUUID := uuid.New().String()
+	_, err = i.oapiClient.UploadAsset(context.Background(), &oapi.AssetMediaCreateDtoMultipart{
+		AssetData: http.MultipartFile{
+			Name:   file.Name(),
+			File:   file,
+			Size:   fileInfo.Size(),
+			Header: mimetype,
+		},
+		DeviceAssetId:  assetUUID,
+		DeviceId:       "apitest",
+		FileCreatedAt:  time.Now(),
+		FileModifiedAt: time.Now(),
+	},
+		oapi.UploadAssetParams{
+			XImmichChecksum: oapi.NewOptString(*assetSha1),
+		})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+type ImmichServerSecuritySource struct {
+	key oapi.APIKey
+}
+
+func (s *ImmichServerSecuritySource) APIKey(ctx context.Context, operationName string) (oapi.APIKey, error) {
+	return s.key, nil
+}
+
+// Bearer provides bearer security value.
+func (s *ImmichServerSecuritySource) Bearer(ctx context.Context, operationName string) (oapi.Bearer, error) {
+	return oapi.Bearer{}, ogenerrors.ErrSkipClientSecurity
+}
+
+// Cookie provides cookie security value.
+func (s *ImmichServerSecuritySource) Cookie(ctx context.Context, operationName string) (oapi.Cookie, error) {
+	return oapi.Cookie{}, ogenerrors.ErrSkipClientSecurity
+}
--- main.go
+++ main.go
@@ -1,21 +1,10 @@
 package main
 
 import (
-	"context"
-	"crypto/sha1"
 	"flag"
 	"fmt"
-	"io"
-	"mime"
-	"net/textproto"
-	"os"
-	"path/filepath"
-	"time"
 
-	"github.com/JonaEnz/immich-sync/oapi"
-	"github.com/google/uuid"
-	"github.com/ogen-go/ogen/http"
-	"github.com/ogen-go/ogen/ogenerrors"
+	"github.com/JonaEnz/immich-sync/immichserver"
 )
 
 var apiKey string
@@ -23,7 +12,9 @@ var apiKey string
 func main() {
 	serverURL := flag.String("url", "http://192.168.0.136:2283/api", "Immich server url with trailing /api")
 	apiKey = *flag.String("api-key", "y2gDkeRqPpiTcM0CpQpTc58hxTutkltzBOHLYYw70", "api key")
-	client, _ := oapi.NewClient(*serverURL, &APIKeySecuritySource{})
+	concurrentUploads := flag.Int("concurrentUploads", 5, "Number of concurrent uploads")
+
+	server := immichserver.NewImmichServer(apiKey, *serverURL)
 
 	imageDir := NewImageDirectory("/home/jona/Pictures/Screenshots")
 	err := imageDir.Read()
@@ -31,13 +22,20 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	sem := make(chan int, *concurrentUploads)
+
 	for imagePath, entry := range imageDir.contentCache {
 		fmt.Printf("%s %x\n", entry.info.Name(), entry.hashSha1)
 		h := entry.HashHexString()
-		err = upload(client, imagePath, &h)
-		if err != nil {
-			fmt.Println(err)
-		}
+		sem <- 1
+		go func(imagePath, h string) {
+			err = server.Upload(imagePath, &h)
+			if err != nil {
+				fmt.Println(err)
+			}
+			<-sem
+		}(imagePath, h)
 	}
 
 	// albums, err := client.GetAllAlbums(context.Background(), oapi.GetAllAlbumsParams{})
@@ -47,69 +45,4 @@ func main() {
 	// for _, a := range albums {
 	// 	fmt.Println(a.AlbumName)
 	// }
-}
-
-func upload(c *oapi.Client, path string, assetSha1 *string) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-
-	if assetSha1 == nil {
-		h := sha1.New()
-		if _, err = io.Copy(h, file); err != nil {
-			return err
-		}
-		sha1String := fmt.Sprintf("%x", h.Sum(nil))
-		assetSha1 = &sha1String
-	}
-
-	fileInfo, err := file.Stat()
-	if err != nil {
-		return err
-	}
-	mimename, _, err := mime.ParseMediaType(filepath.Ext(path))
-	if err != nil {
-		return err
-	}
-	mimetype := textproto.MIMEHeader{}
-	mimetype.Set("Content-Type", mimename)
-	assetUUID := uuid.New().String()
-	_, err = c.UploadAsset(context.Background(), &oapi.AssetMediaCreateDtoMultipart{
-		AssetData: http.MultipartFile{
-			Name:   file.Name(),
-			File:   file,
-			Size:   fileInfo.Size(),
-			Header: mimetype,
-		},
-		DeviceAssetId:  assetUUID,
-		DeviceId:       "apitest",
-		FileCreatedAt:  time.Now(),
-		FileModifiedAt: time.Now(),
-	},
-		oapi.UploadAssetParams{
-			XImmichChecksum: oapi.NewOptString(*assetSha1),
-		})
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-type APIKeySecuritySource struct{}
-
-func (a *APIKeySecuritySource) APIKey(ctx context.Context, operationName string) (oapi.APIKey, error) {
-	return oapi.APIKey{
-		APIKey: apiKey,
-	}, nil
-}
-
-// Bearer provides bearer security value.
-func (a *APIKeySecuritySource) Bearer(ctx context.Context, operationName string) (oapi.Bearer, error) {
-	return oapi.Bearer{}, ogenerrors.ErrSkipClientSecurity
-}
-
-// Cookie provides cookie security value.
-func (a *APIKeySecuritySource) Cookie(ctx context.Context, operationName string) (oapi.Cookie, error) {
-	return oapi.Cookie{}, ogenerrors.ErrSkipClientSecurity
 }
