814eb02118c8bebbffa442bf939262317216f292
Jona Enzinger <jona_enzinger@outlook.com>
2024-11-23 12:11:09 +0100

    Add daemon mode

--- main.go
+++ main.go
@@ -3,46 +3,59 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	"github.com/JonaEnz/immich-sync/immichserver"
 )
 
-var apiKey string
+var (
+	apiKey            string
+	server            *immichserver.ImmichServer
+	concurrentUploads int
+)
 
 func main() {
 	serverURL := flag.String("url", "http://192.168.0.136:2283/api", "Immich server url with trailing /api")
 	apiKey = *flag.String("api-key", "y2gDkeRqPpiTcM0CpQpTc58hxTutkltzBOHLYYw70", "api key")
-	concurrentUploads := flag.Int("concurrentUploads", 5, "Number of concurrent uploads")
+	concurrentUploads = *flag.Int("concurrentUploads", 5, "Number of concurrent uploads")
+	daemon := flag.Bool("d", false, "Start as daemon ()")
+	scanMins := flag.Int("scan-minutes", 15, "Minutes delay between scans (requires -d)")
 
-	server := immichserver.NewImmichServer(apiKey, *serverURL)
+	server = immichserver.NewImmichServer(apiKey, *serverURL)
 
-	imageDir := NewImageDirectory("/home/jona/Pictures/Screenshots")
-	err := imageDir.Read()
+	i := NewImageDirectory("/home/jona/Pictures/Screenshots")
+	imageDirs := []*ImageDirectory{&i}
+	err := imageDirs[0].Read()
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	sem := make(chan int, *concurrentUploads)
-
-	for imagePath, entry := range imageDir.contentCache {
-		fmt.Printf("%s %x\n", entry.info.Name(), entry.hashSha1)
-		h := entry.HashHexString()
-		sem <- 1
-		go func(imagePath, h string) {
-			err = server.Upload(imagePath, &h)
-			if err != nil {
-				fmt.Println(err)
-			}
-			<-sem
-		}(imagePath, h)
+	if *daemon && *scanMins >= 1 {
+		for {
+			doScan(imageDirs)
+			time.Sleep(time.Minute * time.Duration(*scanMins))
+		}
+	} else {
+		doScan(imageDirs)
 	}
+}
 
-	// albums, err := client.GetAllAlbums(context.Background(), oapi.GetAllAlbumsParams{})
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// for _, a := range albums {
-	// 	fmt.Println(a.AlbumName)
-	// }
+func doScan(imageDirs []*ImageDirectory) {
+	sem := make(chan int, concurrentUploads)
+	for _, dir := range imageDirs {
+		fmt.Printf("Scanning directory %s...\n", dir.path)
+		for imagePath, entry := range dir.contentCache {
+			fmt.Printf("%s %x\n", entry.info.Name(), entry.hashSha1)
+			h := entry.HashHexString()
+			sem <- 1
+			go func(imagePath, h string) {
+				err := server.Upload(imagePath, &h)
+				if err != nil {
+					fmt.Println(err)
+				}
+				<-sem
+			}(imagePath, h)
+		}
+	}
 }
